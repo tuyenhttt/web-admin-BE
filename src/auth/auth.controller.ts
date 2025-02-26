10b30093c9dd2e3741e87a3fd126185b20a7f010
Thanh Tuyền <tuyen13022003@gmail.com>
2025-02-26 21:06:53 +0700

    config send email

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,10 +10,14 @@ import { AuthService } from './auth.service';
 import { LocalAuthGuard } from '@/auth/passport/local-auth.guard';
 import { Public } from '@/decorator/customize';
 import { CreateAuthDto } from '@/auth/dto/create-auth.dto';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly mailerService: MailerService,
+  ) {}
 
   @Post('login')
   @Public()
@@ -26,5 +30,17 @@ export class AuthController {
   @Public()
   register(@Body() registerDto: CreateAuthDto) {
     return this.authService.handleRegister(registerDto);
+  }
+
+  @Get('mail')
+  @Public()
+  testMail() {
+    this.mailerService.sendMail({
+      to: 'tuyenhttqe170226@fpt.edu.vn', // list of receivers
+      subject: 'Testing Nest MailerModule ✔', // Subject line
+      text: 'welcome', // plaintext body
+      html: '<b>Hello world web Thanh Tuyen</b>', // HTML body content
+    });
+    return 'ok';
   }
 }
